22cbd4ff3c3dbc52a108548f50c26103690ade8d
RedPandar <izambezik@gmail.com>
2018-03-15 13:50:45 +0500

    Head/foot/image

--- footer.php
+++ footer.php
@@ -1,1 +1,15 @@
-<h1>footer</h1>
+<style>
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #6995C2;
+    color: white;
+    text-align: center;
+}
+</style>
+
+<div class="footer">
+  <p><strong>The current season is 2017-2018, and the NBA already has a history of 72 years.</strong></p>
+</div>
--- header.php
+++ header.php
@@ -1,1 +1,75 @@
-<h1>header</h1>
+<div class="PageHeader">
+     <hr class="Line" >
+    <input type="submit" class="Button_back" onclick="href='../';" value="Back">
+    <label class="sys_name">NBA Management System</label>
+    <div class="logo_container">
+        <img src="images/logo.jpg" class="logo">
+    </div>
+    <div class="Page_Name_Container">
+        <span><strong>Text</strong></span>
+    </div>
+</div>
+
+<style>
+.PageHeader{
+position:absolute;
+text-align:left;
+left:0px;
+top:0px;
+width:100%;
+height:100px;
+}
+
+.Line{
+ background-color:#6995C2;
+ position:absolute;
+ left:0px;
+ top:100%;
+ width:100%;
+}
+
+.Button_back{
+position:absolute;
+left:90%;
+top:50%;
+width:96px;
+height:25px
+}
+
+.sys_name{
+position:absolute;
+left:53px;
+top:80%;
+width:260px;
+height:23px;
+line-height:23px
+}
+
+.logo_container{
+position:absolute;
+left:1px;
+top:6px;
+width:42px;
+height:89px
+}
+.logo{
+position:absolute;
+left:10%;
+top:1%;
+width:50px;
+height:98px
+}
+
+.Page_Name_Container{
+position:absolute;
+left:45%;
+top:70%;
+width:179px;
+height:32px
+}
+
+span{
+color:#000000;
+font-size:27px;
+}
+</style>

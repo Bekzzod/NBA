78378e86fc3968a947877d4d2bfb56e54932f742
RedPandar <izambezik@gmail.com>
2018-03-20 15:28:18 +0500

    Visitors_menu(bad code)

---
+++ Visitor_menu.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Page</title>
+<link href="css/jquery-ui.min.css" rel="stylesheet">
+<link href="css/projectcss.css" rel="stylesheet">
+<link href="css/Visitor_menu.css" rel="stylesheet">
+<script src="js/jquery-1.12.4.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script>
+$(document).ready(function()
+{
+   $("#jQueryButton1").button();
+   $("#jQueryButton2").button();
+   $("#jQueryButton3").button();
+   $("#jQueryButton4").button();
+});
+</script>
+</head>
+<body>
+<div id="container">
+<div id="Layer1" style="position:absolute;text-align:left;left:24px;top:149px;width:1202px;height:667px;z-index:11;">
+<div id="wb_jQueryButton1" style="position:absolute;left:130px;top:84px;width:312px;height:140px;z-index:5;">
+<a class="ui-button ui-widget ui-corner-all" href="./Teams_Main.html" id="jQueryButton1" style="width:100%;height:100%;">Teams</a></div>
+<div id="wb_jQueryButton2" style="position:absolute;left:733px;top:84px;width:312px;height:140px;z-index:6;">
+<a class="ui-button ui-widget ui-corner-all" href="./Players_Main.html" id="jQueryButton2" style="width:100%;height:100%;">Players</a></div>
+<div id="wb_jQueryButton4" style="position:absolute;left:733px;top:423px;width:312px;height:140px;z-index:7;">
+<a class="ui-button ui-widget ui-corner-all" href="" id="jQueryButton4" style="width:100%;height:100%;">Photos</a></div>
+<div id="wb_jQueryButton3" style="position:absolute;left:130px;top:423px;width:312px;height:140px;z-index:8;">
+<a class="ui-button ui-widget ui-corner-all" href="" id="jQueryButton3" onclick="window.location.href='./Matchup_list.html';return false;" style="width:100%;height:100%;">Matchups</a></div>
+</div>
+</div>
+</body>
+</html>

2991300082eb64cdc279f7f4e0676517981ea842
topokama <facilisdes@gmail.com>
2018-04-27 00:12:43 +0500

    index

--- core.php
+++ core.php
@@ -1,5 +1,6 @@
 <?php
 include('core_functions.php');
-NBACore::ShowHeader();
+if(!in_array($_SERVER['REQUEST_URI'], NBACore::$pagesWithNoHeader))
+    NBACore::ShowHeader();
 NBACore::ShowRequestedPage($_SERVER['DOCUMENT_ROOT'] . $_SERVER['REQUEST_URI']);
 NBACore::ShowFooter();

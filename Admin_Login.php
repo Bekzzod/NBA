554b74aa5b405e182c97cbf987bd8dc5c3dece69
mars1996 <marselmustafin19966@mail.ru>
2018-05-07 10:52:23 +0500

    marsel

--- Admin_Login.php
+++ Admin_Login.php
@@ -2,50 +2,46 @@
     NBACore::SetProperty('title', 'Login page');
     NBACore::FillProperty('css', '/css/Admin_Login.css');
     NBACore::FillProperty('js', '/js/wwb12.min.js');
+    NBACore::FillProperty('js', '/js/AdminJS.js');
     ?>
 
     <div id="container">
-        <div id="Login1" style="position:absolute;left:275px;top:248px;width:807px;height:448px;z-index:10;">
-            <form name="loginform" method="post" accept-charset="UTF-8" id="loginform">
-                <input type="hidden" name="form_name" value="loginform">
-                <table id="Login1">
-                    <tr>
-                        <td class="header">Log In</td>
-                    </tr>
-                    <tr>
-                        <td class="label"><label for="username">User Name</label></td>
-                    </tr>
-                    <tr>
-                        <td class="row"><input class="input" name="username" type="text" id="username" value=""></td>
-                    </tr>
-                    <tr>
-                        <td class="label"><label for="password">Password</label></td>
-                    </tr>
-                    <tr>
-                        <td class="row"><input class="input" name="password" type="password" id="password" value=""></td>
-                    </tr>
-                    <tr>
-                        <td class="row"><input id="rememberme" type="checkbox" name="rememberme"><label for="rememberme">Remember me</label></td>
-                    </tr>
-                    <tr>
-                        <td style="text-align:center;vertical-align:bottom"><input class="button" type="submit" name="login" value="Log in" id="login"></td>
-                    </tr>
-                </table>
-            </form>
-        </div>
-        <div id="Text3" style="position:absolute;left:366px;top:187px;width:625px;height:22px;z-index:13;">
-             <span style="color:#2B2B2B;font-family:Arial;font-size:19px;">Users can login into the system using their jobnumber and password.</span>
-        </div>
-        <div id="Layer1" style="position:absolute;text-align:left;visibility:hidden;left:301px;top:231px;width:707px;height:454px;z-index:14;">
-            <div id="Text4" style="position:absolute;left:236px;top:57px;width:246px;height:24px;z-index:4;">
-                <span style="color:#FFD700;font-family:Arial;font-size:21px;"><strong>FOR TESTING ONLY!</strong></span>
+        <div class="content">
+            <div id="Text3" >
+                <span>Users can login into the system using their jobnumber and password.</span>
             </div>
-            <div id="Text5" style="position:absolute;left:221px;top:95px;width:261px;height:36px;text-align:center;z-index:5;">
-                <span style="color:#000000;font-family:Arial;font-size:16px;">Which type of user would you like to <br>log in to the system as?</span>
+            <div class="content-log-in">
+                <div class="header">Log In</div>
+                <div class="form">
+                    <form name="loginform" method="post" accept-charset="UTF-8" id="loginform">
+                        <input type="hidden" name="form_name" value="loginform">
+                        <div class="form-group">
+                            <label for="email">User Name:</label>
+                            <input  class="form-control" name="username" type="text" id="username" value="">
+                        </div>
+                        <div class="form-group">
+                            <label for="pwd">Password:</label>
+                            <input name="password" type="password" id="password" value="" class="form-control" id="pwd">
+                        </div>
+                        <div class="conteiner-button"> 
+                            <input type="submit" class="btn btn-default button-log-in" value="Log in"/>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <button type="submit" id="jQueryButton1" onclick="ShowObject('Layer1', 0);return false;" name="" value="Event Administrator">Event Administrator</button>
-            <button type="submit" id="jQueryButton2" onclick="ShowObject('Layer1', 0);return false;" name="" value="Technical Administrator">Technical Administrator</button>
-            <button type="reset" id="jQueryButton3" onclick="window.location.href='./index.php';return false;" name="" value="Back" style="position:absolute;left:631px;top:10px;width:63px;height:47px;z-index:8;">Back</button>
+
+            <div id="Layer1" >
+                <div id="Text4" >
+                    <span><strong>FOR TESTING ONLY!</strong></span>
+                </div>
+                <div id="Text5">
+                    <span >Which type of user would you like to <br>log in to the system as?</span>
+                </div>
+                <input type="submit" name="" class="btn btn-default button-log-in"  value="Event Administrator"/>
+                <input type="submit" name="" class="btn btn-default button-log-in"  value="Technical Administrator"/>
+                <a href="#" class="Layer1-close">X</a>
+            </div>
+
         </div>
     </div>
 
--- header.php
+++ header.php
@@ -6,10 +6,16 @@
     <link href="css/jquery-ui.min.css" rel="stylesheet">
     <link href="css/projectcss.css" rel="stylesheet">
     <link href="css/Main.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/jquery-1.12.4.min.js"></script>
     <!--##__CSS__##-->
 </head>
 <body>
-
+<?php
+    NBACore::FillProperty('css', '/css/jquery-ui.min.css');
+    NBACore::FillProperty('js', '/js/header.js');
+    NBACore::FillProperty('css', '/css/header.css');
+?>
 <div class="PageHeader">
     <div class="PageHeader-content">
         <input type="submit" class="Button_back" onclick="goBack()" value="Back">
@@ -22,76 +28,3 @@
     </div>
     <hr class="Line">
 </div>
-<script>
-    function goBack(){
-        window.history.back();
-        console.log("back");
-    }
-</script>
-<style>
-.PageHeader{
-position:relative;
-text-align:left;
-width:100%;
-height:100px;
-}
-
-.PageHeader-content{
-position:relative;    
-height:100%;
-}
-
-.Line{
- background-color:#6995C2;
- width:100%;
- border:0;
-}
-
-.Button_back{
-    position: absolute;
-    right: 50px;
-    top: 50%;
-    transform: translate(0%,-50%);
-    width: 100px;
-    font-size: 20px;
-    padding: 10px;
-    border-radius: 10px;
-    width: 130px;
-    background: #c3c3c3;
-}
-.PageHeader-content-logo{
-    position:relative;
-    left:10px;
-    height:90%;
-    top:50%;
-    transform: translate(0%,-50%);  
-    width:50%;
-    display:flex;
-}
-.PageHeader-content-logo>.text{
-    color:#ada9a9;
-    font-size:27px;
-    position:relative;  
-    top: 100%;
-    height: 30px;
-    transform: translate(0%,-120%);  
-}
-.PageHeader-content-logo>.logo{
-    background:url('images/logo.jpg');
-    height:100%;
-    width:80px;    
-    background-repeat: no-repeat;
-    background-size: contain;
-}
-
-#pageName{
-position:absolute;
-left:50%;
-top:50%;
-    transform: translate(-50%,0%);
-    height: 32px;
-    color:#2f2f2f;
-    font-size:3em;
-    font-weight:700;
-}
-</style>
